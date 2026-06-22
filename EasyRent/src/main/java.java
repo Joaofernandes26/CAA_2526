a5322b89c18ba8cefc596cfbf750eb8fb682e2b2
Natalino-Semedeo <nsemedo.l22@us.edu.cv>
2026-06-22 22:38:34 -0100

    EasyRent-23-06-26-22:38

---
+++ EasyRent/src/main/java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author Natalino PC
+ */
+public class java {
+    public static void main(String[] args) {
+        
+    }
+    
+}
